26275fad23ae277b9ffc9a3e5761dd751f9d1f67
Sakib Khan <37256061+SakibKhan1@users.noreply.github.com>
2022-03-30 20:58:48 -0400

    Create indent.cpp

---
+++ indent.cpp
@@ -0,0 +1,62 @@
+/*
+Author: Sakib Khan 
+Course: CSCI-135
+Instructor: Tong Yi
+Assignment: Indent
+Write a new program that 
+enhances the program from the previous task. 
+As it reads the input line by line, it should 
+also count the number of open and closed { } in 
+it, and keep track of how many blocks is currently 
+open at the beginning of each line.
+*/
+
+#include <iostream>
+#include <cctype>
+using namespace std;
+
+string removeLeadingSpaces(string line)
+{
+    int i = 0;
+    while (isspace(line[i]))
+        i++;
+        
+    return line.substr(i);
+}
+
+int countChar(string line, char c)
+{
+    int count = 0;
+    for (int i = 0; i < line.length(); i++)
+    {
+        if (line[i] == c)
+            count ++;
+    }
+    return count;
+}
+
+void printLine(string line, int indent) // returns nothing because it prints
+{
+    for (int i = 0; i < indent; i++)
+        cout << '\t';
+
+    cout << line << endl;
+}
+
+int main()
+{
+    string line;
+    int openblocks = 0;
+    while (getline(cin, line)) // for input with spaces 
+    {
+        line = removeLeadingSpaces(line);
+
+        if (line[0] == '}')
+            printLine(line, openblocks-1);
+        else
+            printLine(line, openblocks);
+
+        openblocks += countChar(line, '{') - countChar(line, '}');
+    }
+    return 0;
+}
